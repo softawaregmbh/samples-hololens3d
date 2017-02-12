7e5de4df80aeb83db11b4f1e95f2eb7cafcb3df3
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2017-02-12 17:35:47 +0100

    Added input and shooting

---
+++ Assets/ShootBall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour, IInputClickHandler
+{
+    public GameObject ball;
+    public float force = 100;
+
+    public void Start()
+    {
+        InputManager.Instance.PushModalInputHandler(gameObject);
+    }
+
+    public void OnInputClicked(InputEventData eventData)
+    {
+        var shot = Instantiate(ball);
+        shot.transform.position = Camera.main.transform.position;
+
+        shot.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * force);
+    }
+}
